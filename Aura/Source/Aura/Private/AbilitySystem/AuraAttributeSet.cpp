92ec1f3e06dd445401806f7d239ef74c5f53d92e
Izen <jherraizmarti@gmail.com>
2023-11-04 23:35:57 +0100

    Clamp fix

--- Aura/Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
+++ Aura/Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -47,6 +47,14 @@ void UAuraAttributeSet::PostGameplayEffectExecute(const struct FGameplayEffectMo
 	FEffectProperties Props;
 	SetEffectProperties(Data, Props);
 
+	if (Data.EvaluatedData.Attribute == GetHealthAttribute())
+	{
+		SetHealth(FMath::Clamp(GetHealth(), 0.f, GetMaxHealth()));
+	}
+	if (Data.EvaluatedData.Attribute == GetManaAttribute())
+	{
+		SetHealth(FMath::Clamp(GetMana(), 0.f, GetMaxMana()));
+	}
 }
 
 void UAuraAttributeSet::SetEffectProperties(const struct FGameplayEffectModCallbackData& Data, FEffectProperties& Props) const
