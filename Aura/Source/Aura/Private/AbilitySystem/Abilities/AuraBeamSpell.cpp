bd6fdb2d672b0322f2d953133d5daedeb1a4ab9f
Izen/Laptop <jherraizmarti@gmail.com>
2024-07-20 21:58:18 +0200

    First Trace Target

--- Aura/Source/Aura/Private/AbilitySystem/Abilities/AuraBeamSpell.cpp
+++ Aura/Source/Aura/Private/AbilitySystem/Abilities/AuraBeamSpell.cpp
@@ -3,6 +3,13 @@
 
 #include "AbilitySystem/Abilities/AuraBeamSpell.h"
 #include "GameFramework/Character.h"
+#include "Interaction/CombatInterface.h"
+#include "Kismet/KismetSystemLibrary.h"
+
+namespace
+{
+	constexpr float SPHERE_TRACE_RADIUS = 10.f;
+}
 
 void UAuraBeamSpell::StoreMouseDataInfo(const FHitResult HitResult)
 {
@@ -23,5 +30,29 @@ void UAuraBeamSpell::StoreOwnerParams()
 	{
 		OwnerPlayerController = CurrentActorInfo->PlayerController.Get();
 		OwnerCharacter = Cast<ACharacter>(CurrentActorInfo->AvatarActor);
+	}
+}
+
+void UAuraBeamSpell::TraceFrontmostTarget(const FVector& TargetLocation)
+{
+	check(OwnerCharacter);
+	// TODO: use GetCombatSocketLocation
+	if (OwnerCharacter->Implements<UCombatInterface>())
+	{
+		if (const USkeletalMeshComponent* Weapon = ICombatInterface::Execute_GetWeapon(OwnerCharacter))
+		{
+			const TArray<AActor*> ActorsToIgnore{OwnerCharacter};
+			FHitResult HitResult;
+			const FVector SocketLocation = Weapon->GetSocketLocation(FName("TipSocket"));
+			UKismetSystemLibrary::SphereTraceSingle(OwnerCharacter, SocketLocation, TargetLocation, SPHERE_TRACE_RADIUS,
+			                                        TraceTypeQuery1, false, ActorsToIgnore,
+			                                        /*EDrawDebugTrace::ForDuration*/ EDrawDebugTrace::None, HitResult, true);
+
+			if (HitResult.bBlockingHit)
+			{
+				MouseHitLocation = HitResult.ImpactPoint;
+				TargetActor = HitResult.GetActor();
+			}
+		}
 	}
 }
--- Aura/Source/Aura/Public/AbilitySystem/Abilities/AuraBeamSpell.h
+++ Aura/Source/Aura/Public/AbilitySystem/Abilities/AuraBeamSpell.h
@@ -21,6 +21,9 @@ public:
 
 	UFUNCTION(BlueprintCallable)
 	void StoreOwnerParams();
+
+	UFUNCTION(BlueprintCallable)
+	void TraceFrontmostTarget(const FVector& TargetLocation);
 	
 protected:
 
